5d2f5a0fd0917d649969a27287dc34ac95a9a2f6
Bleart Hyseni <bleart.hyseni@umib.net>
2026-06-10 23:42:09 +0200

    Register auth routes

---
+++ app/main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.database import Base, engine
+from app.api.auth import router as auth_router
+from app.models.user import User
+
+load_dotenv()
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="PulseForm API")
+
+origins = os.getenv("CORS_ORIGINS", "http://localhost:5173").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "PulseForm API is running"}
+
+
+app.include_router(auth_router)

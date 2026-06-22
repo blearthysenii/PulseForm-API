fb03b354f9a67a37e830b31f2b42a6c427dcb703
Elion Rexhepi <114259064+ElionR@users.noreply.github.com>
2026-06-22 16:51:42 +0200

    Questions fixed for front end

--- app/schemas/question.py
+++ app/schemas/question.py
@@ -1,21 +1,23 @@
 from pydantic import BaseModel
-from typing import Optional
- 
- 
+from typing import Optional, List
+
+
 class QuestionCreate(BaseModel):
     text: str
-    type: str  # mcq | rating | text
+    type: str  # single_choice | multiple_choice | mcq | rating | text
     is_required: Optional[bool] = False
     position: Optional[int] = 0
- 
- 
+    options: Optional[List[str]] = []
+
+
 class QuestionUpdate(BaseModel):
     text: Optional[str] = None
     type: Optional[str] = None
     is_required: Optional[bool] = None
     position: Optional[int] = None
- 
- 
+    options: Optional[List[str]] = None
+
+
 class QuestionResponse(BaseModel):
     id: int
     survey_id: int
@@ -23,7 +25,7 @@ class QuestionResponse(BaseModel):
     type: str
     is_required: bool
     position: int
- 
+    options: List[str] = []
+
     class Config:
         from_attributes = True
- 
